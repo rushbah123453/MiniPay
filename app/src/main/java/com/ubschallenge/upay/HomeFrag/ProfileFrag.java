dd0ad653bdafe035845dc3c7079583708183c872
rushabh123453@gmail.com <rushabh123453@gmail.com>
2018-10-07 09:20:32 +0530

    Profile Backend Integration with UI Done

--- app/src/main/java/com/ubschallenge/upay/HomeFrag/ProfileFrag.java
+++ app/src/main/java/com/ubschallenge/upay/HomeFrag/ProfileFrag.java
@@ -26,7 +26,7 @@ import org.json.JSONObject;
  */
 public class ProfileFrag extends Fragment implements AsyncResponse{
 
-    TextView logout;
+    TextView logout,balance,email,phone,adhar,name;
     View view;
 
 
@@ -40,6 +40,12 @@ public class ProfileFrag extends Fragment implements AsyncResponse{
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view= inflater.inflate(R.layout.fragment_profile, container, false);
+
+        balance=(TextView)view.findViewById(R.id.balanceprofile);
+        phone=(TextView)view.findViewById(R.id.phoneprofile);
+        email=(TextView)view.findViewById(R.id.emailprofile);
+        adhar=(TextView)view.findViewById(R.id.adharprofile);
+        name=(TextView)view.findViewById(R.id.nameprofile);
 
 
         logout=(TextView)view.findViewById(R.id.logout);
@@ -89,9 +95,21 @@ public class ProfileFrag extends Fragment implements AsyncResponse{
         Toast.makeText(getContext(),"Profile"+output,Toast.LENGTH_SHORT).show();
         JSONObject jsonObject=new JSONObject(output);
         JSONObject response_data=jsonObject.getJSONObject("response_data");
-        String name=response_data.getString("u_name");
+        String name_response=response_data.getString("u_name");
+        String balance_response=response_data.getString("balance");
+        String phone_response=response_data.getString("phone");
+        String aadhaar_response=response_data.getString("aadhaar");
+        String email_response=response_data.getString("email");
 
-        Toast.makeText(getContext(),"name"+name,Toast.LENGTH_SHORT).show();
+
+        balance.setText(balance_response);
+        phone.setText(phone_response);
+        email.setText(email_response);
+        adhar.setText(aadhaar_response);
+        name.setText(name_response);
+
+
+       // Toast.makeText(getContext(),"name"+name,Toast.LENGTH_SHORT).show();
 
     }
 }
